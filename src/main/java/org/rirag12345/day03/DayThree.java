573e672b019c2599c424be0274914e517f9e6b48
Philip Neuffer <pne@itscope.de>
2025-12-25 18:26:18 +0100

    feat: add day three part 1

---
+++ src/main/java/org/rirag12345/day03/DayThree.java
@@ -0,0 +1,38 @@
+package org.rirag12345.day03;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+
+public class DayThree {
+	public static int solve() throws IOException {
+		var joltages = Files.readAllLines(Path.of("src/main/java/org/rirag12345/day03/input.txt"));
+		return joltages.stream().map(DayThree::getLargest).reduce(0, Integer::sum);
+	}
+
+	/**
+	 * Sorts a given input String of numbers and return the highest possible joltage
+	 *
+	 * @param input the input String e.g. "811111111111119"
+	 * @return the highest possible value e.g. "89"
+	 */
+	private static int getLargest(String input) {
+		var result = 0;
+		var values = Arrays.stream(input.split(""))
+				.map(Integer::parseInt)
+				.toList();
+
+		for (int i = 0; i < values.size() - 1; i++) {
+			for (int j = i + 1; j < values.size(); j++) {
+				var first = values.get(i);
+				var second = values.get(j);
+				if ((first * 10 + second) > result) {
+					result = first * 10 + second;
+				}
+			}
+		}
+		// Combine the two highest digits into a two-digit number
+		return result;
+	}
+}
---
+++ src/test/java/day03/TestDay03.java
@@ -0,0 +1,17 @@
+package day03;
+
+import org.junit.jupiter.api.Test;
+import org.rirag12345.day03.DayThree;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class TestDay03 {
+	@Test
+	public void testSolve() throws IOException {
+		var result = DayThree.solve();
+		assertThat(result, is(16973));
+	}
+}
